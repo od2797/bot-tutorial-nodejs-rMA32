fafd8181b48a615afccc56d78ab2768140f91e6f
od2797 <odebs2797@yahoo.com>
2016-12-07 18:48:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ function respond() {
     this.res.writeHead(200);
     postMessage("https://i.groupme.com/640x364.jpeg.f8a0e216bf7e42ed9a86b3ae23cc2ff1.large");
     this.res.end();
-  } 
+  }  
   else if(request.text && botRegexStop.test(request.text)) {
     this.res.writeHead(200);
     postMessage("Stop appropriating black culture");
