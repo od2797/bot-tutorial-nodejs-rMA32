b226c5e8cd0a7e62fecd26d10d742d426e683579
od2797 <odebs2797@yahoo.com>
2016-12-07 22:48:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
       botRegexSlut = /^\/youtube/; botRegexStop = /^\/cheese/;
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/bowl/; botRegexBrye = /^\/forum/;
       botRegexGian = /^\/cheeks/; botRegexScrotes = /^\/buns/; botRegexShaki = /^\/slaw/;
-      botRegexDaf = /^\/dafuq/; botRegexAyy = /^\/ayy/; botRegexSchedule = /^\/schedule/;
+      botRegexDaf = /^\/dafuq/; botRegexAyy = /^\/ayy/; botRegexSchedule = /^\/booty/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -162,7 +162,7 @@ function respond() {
   }
   else if(request.text && botRegexSchedule.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(" ");
+    postMessage("http://i.imgur.com/4vwmAtL.jpg");
     this.res.end();
   }
   
